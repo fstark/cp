fce559c2ce5f550fa4c43de9298d1698879802c6
Frederic Stark <frederic.stark@gmail.com>
2020-05-16 15:45:34 +0200

    Ready to compete!

---
+++ 2020/2/22.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long T;
+    long X;
+    long Y;
+    cin >> T;
+
+    for (long casen=1;casen<=T;casen++)
+    {
+        cin >> X;
+        cin >> Y;
+
+		cout << "Case #" << casen << ": ";
+
+        cout << X << "\n";
+    }
+}
